f25fe157832d7c48458548c457154f1ab0cca250
Chris Grieger <chris@scolp.de>
2015-01-19 14:30:27 +0100

    Originalquelle hinzufügen

--- chat_client.py
+++ chat_client.py
@@ -6,43 +6,44 @@
 #
 # author: gnivciv
 # last edit: 18.01-2015
+# original source: http://www.binarytides.com/code-chat-application-server-client-sockets-python/
 #
 
 import socket, select, string, sys
- 
+
 def prompt() :
     sys.stdout.write('<You> ')
     sys.stdout.flush()
- 
+
 #main function
 if __name__ == "__main__":
-     
+
     if(len(sys.argv) < 3) :
         print 'python chat_server.py ip port'
         sys.exit()
-     
+
     host = sys.argv[1]
     port = int(sys.argv[2])
-     
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
-     
+
     # connect to remote host
     try :
         s.connect((host, port))
     except :
         print 'Keine Verbindung zum Server'
         sys.exit()
-     
+
     print 'Verbunden mit Server. Beginne Nachrichten zu schreiben.'
     prompt()
-     
+
     while 1:
         socket_list = [sys.stdin, s]
-         
+
         # Get the list sockets which are readable
         read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-         
+
         for sock in read_sockets:
             #incoming message from remote server
             if sock == s:
@@ -54,7 +55,7 @@ if __name__ == "__main__":
                     #print data
                     sys.stdout.write(data)
                     prompt()
-             
+
             #user entered a message
             else :
                 msg = sys.stdin.readline()
--- chat_server.py
+++ chat_server.py
@@ -7,6 +7,7 @@
 # author: gnivciv
 # version: 1.0
 # last edit: 18.01.2015
+# original source: http://www.binarytides.com/code-chat-application-server-client-sockets-python/
 #
 
 import socket
@@ -25,28 +26,28 @@ def broadcast_data (sock, message):
                 socket.send(message)
             except :
                 socket.close()
-                CONNECTION_LIST.remove(socket) 
+                CONNECTION_LIST.remove(socket)
 
 if __name__ == "__main__":
-     
+
     CONNECTION_LIST = []
     RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
     PORT = port
-     
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("0.0.0.0", PORT))
     server_socket.listen(10)
- 
+
     # Add server socket to the list of readable connections
     CONNECTION_LIST.append(server_socket)
- 
+
     print "Server wurde gestartet, Port: " + str(PORT)
- 
+
     while 1:
         # Get the list sockets which are ready to be read through select
         read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
- 
+
         for sock in read_sockets:
             #Neue Verbindung
             if sock == server_socket:
@@ -54,22 +55,22 @@ if __name__ == "__main__":
                 sockfd, addr = server_socket.accept()
                 CONNECTION_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
-                 
+
                 broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
-             
+
             #Some incoming message from a client
             else:
                 # Daten vom Client
                 try:
                     data = sock.recv(RECV_BUFFER)
                     if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)               
-                 
+                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
+
                 except:
                     broadcast_data(sock, "Client (%s, %s) ist offline" % addr)
                     print "Client (%s, %s) is offline" % addr
                     sock.close()
                     CONNECTION_LIST.remove(sock)
                     continue
-     
+
     server_socket.close()
