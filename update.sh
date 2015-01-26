b6203def2456459699deeb804da46aa64a6089ae
gnivciv <niels.grieger@gmail.com>
2015-01-26 18:26:30 +0000

    Fixed issue

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-killall python && git pull https://github.com/gnivciv/Chat_Programm/ > log.txt && ./start_server.sh 
+cd chatserver/ && killall python && git pull https://github.com/gnivciv/Chat_Programm/ > log.txt && ./start_server.sh 
