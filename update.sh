3ede8817e2dc00db331601761b904a72ee15a2e0
gnivciv <niels.grieger@gmail.com>
2015-01-29 09:12:02 +0000

    Fixed cd issue

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-ch chatserver/ && killall python && git pull https://github.com/gnivciv/Chat_Programm/ > update_log.txt && ./start_server.sh 
+cd chatserver/ && killall python && git pull https://github.com/gnivciv/Chat_Programm/ > update_log.txt && ./start_server.sh 
